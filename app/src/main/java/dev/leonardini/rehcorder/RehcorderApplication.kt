3960c6cfb40b0038486d7ad80b0f8c40aad56259
LorenzoLeonardini <lorenzo@leonardini.dev>
2022-06-12 15:45:10 +0200

    First commit

---
+++ app/src/main/java/dev/leonardini/rehcorder/RehcorderApplication.kt
@@ -0,0 +1,11 @@
+package dev.leonardini.rehcorder
+
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+
+class RehcorderApplication :Application() {
+    override fun onCreate() {
+        super.onCreate()
+        DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+}
