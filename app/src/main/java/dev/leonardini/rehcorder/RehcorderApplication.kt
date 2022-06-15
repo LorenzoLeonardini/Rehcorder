ac693ba76723644b35d4f92c410299c24e20b230
LorenzoLeonardini <lorenzo@leonardini.dev>
2022-06-15 20:14:16 +0200

    Added ffmpeggit add -A

--- app/src/main/java/dev/leonardini/rehcorder/RehcorderApplication.kt
+++ app/src/main/java/dev/leonardini/rehcorder/RehcorderApplication.kt
@@ -1,11 +1,34 @@
 package dev.leonardini.rehcorder
 
 import android.app.Application
+import android.util.Log
 import com.google.android.material.color.DynamicColors
+import java.lang.String
+import com.arthenica.ffmpegkit.FFmpegKit
+import com.arthenica.ffmpegkit.FFmpegSession
+import com.arthenica.ffmpegkit.ReturnCode
 
 class RehcorderApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        val session: FFmpegSession = FFmpegKit.execute("-version")
+        if (ReturnCode.isSuccess(session.returnCode)) {
+            // SUCCESS
+            Log.i("FFMPEG", session.allLogsAsString)
+        } else if (ReturnCode.isCancel(session.returnCode)) {
+            // CANCEL
+        } else {
+            Log.d(
+                "FFMPEG",
+                String.format(
+                    "Command failed with state %s and rc %s.%s",
+                    session.state,
+                    session.returnCode,
+                    session.failStackTrace
+                )
+            )
+        }
     }
 }
