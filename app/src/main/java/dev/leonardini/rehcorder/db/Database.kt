cbbd76ab3a362986ad95d8fe6a923be970dc7140
LorenzoLeonardini <lorenzo@leonardini.dev>
2022-07-29 11:56:04 +0200

    Removed support for db 2->1 migration

--- app/src/main/java/dev/leonardini/rehcorder/db/Database.kt
+++ app/src/main/java/dev/leonardini/rehcorder/db/Database.kt
@@ -11,31 +11,13 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 object Database {
     private lateinit var database: AppDatabase
 
-    // Location branch, this migration will be deleted in the future
-    private val MIGRATION_2_1 = object : Migration(2, 1) {
-        override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE rehearsal RENAME TO rehearsal_old")
-            database.execSQL(
-                "CREATE TABLE rehearsal (" +
-                        "uid INTEGER NOT NULL PRIMARY KEY, name TEXT, status INTEGER NOT NULL DEFAULT(${Rehearsal.CREATED}), " +
-                        "date INTEGER NOT NULL, file_name TEXT NOT NULL, external_storage INTEGER NOT NULL" +
-                        ")"
-            )
-            database.execSQL(
-                "INSERT INTO rehearsal (uid, name, status, date, file_name, external_storage) " +
-                        "SELECT uid, name, status, date, file_name, external_storage FROM rehearsal_old"
-            )
-            database.execSQL("DROP TABLE rehearsal_old")
-        }
-    }
-
     fun getInstance(applicationContext: Context): AppDatabase {
         if (!::database.isInitialized) {
             database = Room.databaseBuilder(
                 applicationContext,
                 AppDatabase::class.java,
                 "rehearsal-database"
-            ).addMigrations(MIGRATION_2_1).build()
+            ).build()
         }
         return database
     }
